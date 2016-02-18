66c5246cc9628f18740a10ce747d9feb9649069b
sashi <sashib@gmail.com>
2016-02-18 08:28:10 -0800

    added save button

--- Ideas/app/src/main/java/com/appwallah/ideas/NewIdeaActivity.java
+++ Ideas/app/src/main/java/com/appwallah/ideas/NewIdeaActivity.java
@@ -2,12 +2,45 @@ package com.appwallah.ideas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class NewIdeaActivity extends AppCompatActivity {
+
+    NewIdeaController mNewIdeaController;
+
+    Button mSaveButton;
+    EditText mIdeaEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_idea);
+
+        initializeController();
+        initializeView();
+
     }
+
+
+    public void initializeController() {
+        this.mNewIdeaController = new NewIdeaController(this);
+    }
+
+    public void initializeView() {
+        mIdeaEdit = (EditText) findViewById(R.id.idea_edit);
+        mSaveButton = (Button) findViewById(R.id.save_button);
+        mSaveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveIdea();
+            }
+        });
+    }
+
+    public void saveIdea() {
+        mNewIdeaController.saveIdea(mIdeaEdit.getText().toString());
+    }
+
 }
---
+++ Ideas/app/src/main/java/com/appwallah/ideas/NewIdeaController.java
@@ -0,0 +1,15 @@
+package com.appwallah.ideas;
+
+import android.content.Context;
+
+public class NewIdeaController {
+    Context mContext;
+
+    public NewIdeaController(Context ctx) {
+        this.mContext = ctx;
+    }
+
+    public void saveIdea(String txt) {
+
+    }
+}

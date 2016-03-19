898c200431f6195de2caa0d83a81e4b8b0a39b1c
sashi <sashib@gmail.com>
2016-03-18 22:31:29 -0700

    fixed hashtag parser

--- Ideas/app/src/main/java/com/appwallah/ideas/NewIdeaController.java
+++ Ideas/app/src/main/java/com/appwallah/ideas/NewIdeaController.java
@@ -1,6 +1,7 @@
 package com.appwallah.ideas;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.firebase.client.Firebase;
 import com.firebase.client.ServerValue;
@@ -11,6 +12,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class NewIdeaController {
+
+    private static final String TAG = NewIdeaController.class.getName();
+
     Context mContext;
 
     public NewIdeaController(Context ctx) {
@@ -23,17 +27,24 @@ public class NewIdeaController {
         while (hashIndex >= 0 && hashIndex < msg.length()) {
             String str = msg.substring(hashIndex, msg.length());
             int spaceIndex = str.indexOf(" ");
+            spaceIndex = (spaceIndex <= 0) ? str.length() : spaceIndex;
             String hashTag = str.substring(1, spaceIndex);
             if (hashTag.length() > 1) {
                 hashTags.add(hashTag);
             }
-            hashIndex = (str.indexOf("#", 1));
+            msg = str.substring(1);
+            hashIndex = (msg.indexOf("#"));
 
         }
         return hashTags;
     }
 
     public void saveIdea(String desc) {
+
+        ArrayList<String> hashtags = getHashTags(desc);
+
+        Log.d(TAG, "hastags are: " + hashtags);
+
         Firebase ref = new Firebase(FireBaseConstants.FIREBASE_URL);
         Firebase ideaRef = ref.child("ideas");
         Firebase newIdeaRef = ideaRef.push();

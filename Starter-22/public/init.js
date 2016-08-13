89713d201f20f833ddd8809305ac6d0f3005cc9f
Sashi <sbommakanty@kahuna.com>
2016-08-13 11:27:18 -0700

    added my asks and my votes

---
+++ Starter-22/public/init.js
@@ -0,0 +1,8 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCflQpOaO8-tj8gdMzCBbpB9QcG4Acup4I",
+  authDomain: "starter-22.firebaseapp.com",
+  databaseURL: "https://starter-22.firebaseio.com",
+  storageBucket: "starter-22.appspot.com",
+};
+firebase.initializeApp(config);
